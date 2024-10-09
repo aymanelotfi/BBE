dcd4b49275031dce7ccefa42d0216b037da5aa39
Aymane Lotfi <lotfidevers@gmail.com>
2024-10-09 02:51:59 +0200

    adding Trade

---
+++ include/Trade.h
@@ -0,0 +1,15 @@
+
+struct Trade{
+    int timestamp;
+    // best ask price.
+    int best_ap;
+    // ask volume.
+    int ask_volume;
+    // best bid price.
+    int best_bp;
+    // bid volume.
+    int bid_volume;
+    Trade(){
+        
+    }
+};
