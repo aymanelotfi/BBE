890d6c17885aac5603101f0e51e229657bc472c4
Aymane Lotfi <aymane.lotfi@student-cs.fr>
2024-10-10 22:04:27 +0200

    Add trade

--- include/Trade.h
+++ include/Trade.h
@@ -9,7 +9,10 @@ struct Trade{
     int best_bp;
     // bid volume.
     int bid_volume;
-    Trade(){
-        
-    }
+    Trade() = default;
+
+    Trade(int best_ap, int ask_volume, int best_bp, int bid_volume):
+     best_ap(best_ap), ask_volume(ask_volume), best_bp(best_bp), bid_volume(bid_volume){}
 };
+
+
