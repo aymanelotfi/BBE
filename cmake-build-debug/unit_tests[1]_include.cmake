885d9808ef53b3d10d9eb36935b33c12db7bdb18
Aymane Lotfi <aymane.lotfi@student-cs.fr>
2024-10-13 18:07:29 +0200

    update git ignore

--- cmake-build-debug/unit_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/Vikos/CLionProjects/BBE/cmake-build-debug/unit_tests[1]_tests.cmake")
-  include("C:/Users/Vikos/CLionProjects/BBE/cmake-build-debug/unit_tests[1]_tests.cmake")
-else()
-  add_test(unit_tests_NOT_BUILT unit_tests_NOT_BUILT)
-endif()
